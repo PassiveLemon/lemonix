09df7797c64092de529e356a33ea78de59abfcb6
PassiveLemon <lemonl3mn@protonmail.com>
2022-12-16 15:17:38 -0500

    Update .gitattributes

--- Installer.sh
+++ Installer.sh
@@ -11,7 +11,7 @@ sudo mkdir -p /home/lemon/lemontemp/
 sudo chmod 777 /home/lemon/lemontemp/
 pushd /home/lemon/lemontemp/
 
-echo "|| Getting dotfiles ||"
+echo "|| Get dotfiles ||"
 sudo git clone https://github.com/PassiveLemon/lemondots/
 
 cp -r /home/lemon/lemondots/.nix/ /home/lemon/
@@ -25,14 +25,15 @@ sudo cp /home/lemon/.nix/configuration.nix /etc/nixos/configuration.nix
 
 sudo cp /home/lemon/.wallpapers/Reds/Wallpaper\ \(6\).png /home/lemon/.background-image
 
-echo "|| Running sub-scripts ||"
+echo "|| Run sub-scripts ||"
 
 bash /home/lemon/.local/fontsscript.sh
 bash /home/lemon/.local/iconsscript.sh
 bash /home/lemon/.local/themesscript.sh
 
-echo "|| Changing permissions ||"
-sudo chmod u+x /home/lemon/.xinitrc
+echo "|| Change permissions ||"
+sudo chmod 777 /home/lemon/.config
+#sudo chmod u+x /home/lemon/.xinitrc
 sudo chmod u+x /home/lemon/.config/bspwm/bspwmrc
 sudo chmod u+x /home/lemon/.config/sxhkd/sxhkdrc
 
