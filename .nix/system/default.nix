d8a6523075adc822ddf0168efe2be229ac8480e8
PassiveLemon <lemonl3mn@protonmail.com>
2023-01-04 12:36:36 -0500

    Update default.nix

--- .nix/system/default.nix
+++ .nix/system/default.nix
@@ -63,34 +63,33 @@
         defaultSession = "none+bspwm";
         gdm.enable = true;
         };
-      };
       windowManager.bspwm = {
         enable = true;
         configFile = "/home/lemon/.config/bspwm/bspwmrc";
         sxhkd.configFile = "/home/lemon/.config/sxhkd/sxhkdrc";
       };
       videoDrivers = [ "nvidia" ];
-      };
-      picom.enable = true;
-    autorandr = {
-      enable = true;
-      profiles = {
-        "Lemon" = {
-          fingerprint.DP1 = "<EDID>";
-          config.DP1 = {
-            enable = true;
-            primary = true;
-            mode = "1920x1080";
-            rate = "144.00";
+    };
+  picom.enable = true;
+  autorandr = {
+    enable = true;
+    profiles = {
+      "Lemon" = {
+        fingerprint.DP1 = "<EDID>";
+        config.DP1 = {
+          enable = true;
+          primary = true;
+          mode = "1920x1080";
+          rate = "144.00";
         };
       };
-        "Lime" = {
-          fingerprint.DP2 = "<EDID>";
-          config.DP2 = {
-            enable = false;
-            primary = true;
-            mode = "1920x1080";
-            rate = "60.00";
+      "Lime" = {
+        fingerprint.DP2 = "<EDID>";
+        config.DP2 = {
+          enable = false;
+          primary = true;
+          mode = "1920x1080";
+          rate = "60.00";
         };
       };
     };
