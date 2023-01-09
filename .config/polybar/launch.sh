23560f9ad469bec88aea76999d3951b672eef435
PassiveLemon <lemonl3mn@protonmail.com>
2023-01-08 21:06:31 -0500

    Update launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-# Add this script to your wm startup file.
-
-#DIR="$HOME/.config/polybar"
-
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar lemon 2>&1 | tee -a /tmp/polybar.log &
+polybar lemon &
+polybar lemon-left &
 
 echo "Polybar launched..."
