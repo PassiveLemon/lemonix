c8d99c1e32f5a85f3405f99af2cc2027dc1e68b3
PassiveLemon <lemonl3mn@protonmail.com>
2023-01-08 21:02:52 -0500

    Update default.nix

--- .nix/modules/home/desktop/default.nix
+++ .nix/modules/home/desktop/default.nix
@@ -3,11 +3,8 @@
 {
   # Packages
   environment.systemPackages = with pkgs; [
-    xorg.xrandr
-    gnome.gdm
     bspwm sxhkd picom-jonaburg feh
     polybar dunst
     kitty rofi
-#    pkgs.linuxKernel.packages.linux_zen.rtl8821ce
   ];
 }
