467b85890395b944fa4cb19b7858644624bd0907
PassiveLemon <lemonl3mn@protonmail.com>
2023-01-08 21:03:41 -0500

    Update default.nix

--- .nix/modules/home/default.nix
+++ .nix/modules/home/default.nix
@@ -9,6 +9,9 @@
   ];
 
   environment.systemPackages = with pkgs; [
+    xorg.xrandr
+    gnome.gdm
+    xfce.thunar xfce.thunar-volman xfce.thunar-archive-plugin
     gimp obs-studio github-desktop spotify discord steam vscode firefox gparted vlc lxappearance pavucontrol protonvpn-gui megasync htop neofetch
   ];
 }
