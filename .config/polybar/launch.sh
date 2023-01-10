7e56e332dd90f2609dea03e2059b6c83582f2663
PassiveLemon <lemonl3mn@protonmail.com>
2023-01-09 20:41:57 -0500

    Add files via upload

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Add this script to your wm startup file.
+
+#DIR="$HOME/.config/polybar"
+
 # Terminate already running bar instances
 killall -q polybar
 
@@ -9,5 +13,12 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch the bar
 polybar lemon &
 polybar lemon-left &
+
+#2ndmoni=$(xrandr --query | grep 'DP-0')
+#if [[ $2ndmoni = *connected* ]]; then
+#    polybar lemon-left &
+#fi
+
+#polybar lemon 2>&1 | tee -a /tmp/polybar.log &
 
 echo "Polybar launched..."
