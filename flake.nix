15bf7bc83b88b2aa6d789b13657ece3319620762
PassiveLemon <lemonl3mn@protonmail.com>
2023-01-12 18:48:05 -0500

    update

--- flake.nix
+++ flake.nix
@@ -1,25 +1,25 @@
 {
-    description = "PassiveLemon NixOS";
+  description = "PassiveLemon NixOS";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        }
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = {nixpkgs, home-manager, ...}: {
-        defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-        defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
+  outputs = {nixpkgs, home-manager, ...}: {
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+    defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
 
-        homeConfigurations = {
-            "lemon" = home-manager.lib.homeManagerConfiguration {
-                # Note: I am sure this could be done better with flake-utils or something
-                pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+    homeConfigurations = {
+      "lemon" = home-manager.lib.homeManagerConfiguration {
+        # Note: I am sure this could be done better with flake-utils or something
+        pkgs = nixpkgs.legacyPackages.x86_64-darwin;
 
-                modules = [ ./.nix/hosts/lemon-tree ];
-            };
-        };
+        modules = [ ./.nix/hosts/lemon-tree ];
+      };
     };
+  };
 }
