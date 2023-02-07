50c71d64a43d1dd83a4218ed630d0dc73e3836bc
PassiveLemon <lemonl3mn@protonmail.com>
2023-02-06 21:59:53 -0500

    update

--- .nix/hosts/lemon-tree/config/desktop.nix
+++ .nix/hosts/lemon-tree/config/desktop.nix
@@ -2,12 +2,12 @@
   xdg.desktopEntries = {
     gdlauncher = {
       name = "GDLauncher";
-      exec = "appimage-run /home/lemon/Standalone/GDLauncher-linux-setup.AppImage --disable-gpu-sandbox";
+      exec = "appimage-run /home/lemon/Standalone/GDLauncher-linux-setup.AppImage --no-sandbox";
       terminal = false;
     };
     r2modman = {
       name = "r2modman";
-      exec = "appimage-run /home/lemon/Standalone/r2modman.AppImage --disable-gpu-sandbox";
+      exec = "appimage-run /home/lemon/Standalone/r2modman.AppImage --no-sandbox";
       terminal = false;
     };
   };
--- README.md
+++ README.md
@@ -3,5 +3,7 @@ To be used on a NixOS system. </br>
 
 The organization is more focused on system configurations rather than individual modules. Stuff is still being worked on and will probably always be worked on so stuff will change. </br>
 
+Use the Installer.sh script to install. I will not guarantee that it works. The dotscripts.sh only downloads external themes/icons/fonts. </br>
+
 <img src="desktop.png"> </br>
 I will update the image every so often. </br>
