624ada28669a619fe1c7e1c1e4243b1d37f5a5f4
PassiveLemon <lemonl3mn@protonmail.com>
2023-04-27 22:08:42 -0400

    Feature
    Screen dim

--- .nix/modules/config/awesome/config/helpers/locker.lua
+++ .nix/modules/config/awesome/config/helpers/locker.lua
@@ -1,11 +1,15 @@
 local awful = require("awful")
 
 local function locker()
-  awful.spawn("sh -c ' \
+  awful.spawn([[sh -c " \
   playerctl pause; \
+  xrandr --output DP-0 --brightness 0.5; \
+  xrandr --output DP-2 --brightness 0.5; \
   i3lock-fancy-rapid 50 10 -n; \
-  playerctl play; \
-  '")
+  xrandr --output DP-0 --brightness 1; \
+  xrandr --output DP-2 --brightness 1; \
+  sleep 0.7 && playerctl play; \
+  "]])
 end
 
 return { locker = locker }
