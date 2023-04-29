6897df5de90db917316db3b5d9cd1051cf8635cc
PassiveLemon <lemonl3mn@protonmail.com>
2023-04-29 11:13:44 -0400

    Change
    Don't resume media after unlocking. Frankly, it's kinda ananoying. Also updated the picture to actually represent my current deskop. Wallpaper is not accurate to the files but that can be easily changed.

--- .nix/modules/config/awesome/config/helpers/locker.lua
+++ .nix/modules/config/awesome/config/helpers/locker.lua
@@ -8,7 +8,6 @@ local function locker()
   i3lock-fancy-rapid 50 10 -n; \
   xrandr --output DP-0 --brightness 1; \
   xrandr --output DP-2 --brightness 1; \
-  sleep 0.7 && playerctl play; \
   "]])
 end
 
