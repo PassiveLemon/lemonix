9e2b96ac8acf3a0e968d213af83c9316d8c6b314
PassiveLemon <lemonl3mn@protonmail.com>
2023-05-02 20:05:43 -0400

    Reformat to use helpers

--- .nix/modules/config/awesome/config/widgets/resourcemenu.lua
+++ .nix/modules/config/awesome/config/widgets/resourcemenu.lua
@@ -3,6 +3,7 @@ local gears = require("gears")
 local beautiful = require("beautiful")
 local wibox = require("wibox")
 
+local helpers = require("config.helpers.helpers")
 local click_to_hide = require("config.helpers.click_to_hide")
 
 --
@@ -10,175 +11,43 @@ local click_to_hide = require("config.helpers.click_to_hide")
 --
 -- Special thanks to ChatGPT for the bulk of the commands.
 
-local space = wibox.widget {
-  widget = wibox.widget.textbox,
-  text = " ",
-  align = "center",
-  valign = "center",
-}
+local space = helpers.text(" ", beautiful.font, "center")
 
-local strg_text = wibox.widget {
-  widget = wibox.widget.textbox,
-  text = "Storage",
-  align = "center",
-  valign = "center",
-}
+local cpu_text = helpers.text("CPU", beautiful.font, "center")
 
-local strg_free_nvme = wibox.widget {
-  widget = wibox.widget.textbox,
-  align = "left",
-  valign = "center",
-}
+local cpu_use = helpers.simplewtch([[sh -c "echo -n 'CPU Usage: ' && top -bn1 | grep '%Cpu' | awk '{print int(100-$8)}' && echo -n '%'"]], 1)
 
-awful.widget.watch([[sh -c "echo -n 'NVME: ' && df -h /dev/nvme0n1p3 | awk 'NR==2 {split(\$3, used, \"G\"); split(\$2, total, \"G\"); print used[1] \"/\" total[1] \" GB \" int((used[1]/total[1])*100) \"%\"}'"]], 10, function(_, stdout)
-  strg_free_nvme.text = stdout:gsub( "\n", "" )
-end)
+local cpu_temp = helpers.simplewtch([[sh -c "cat /sys/class/thermal/thermal_zone*/temp | sed 's/\(.*\)000$/\1/' | tr -d '\n' && echo 'C'"]], 2)
 
-local strg_free_sdb = wibox.widget {
-  widget = wibox.widget.textbox,
-  align = "left",
-  valign = "center",
-}
+local mem_text = helpers.text("Memory", beautiful.font, "center")
 
-awful.widget.watch([[sh -c "echo -n 'SDB: ' && df -h /dev/sdb1 | awk 'NR==2 {split(\$3, used, \"T\"); split(\$2, total, \"T\"); print used[1] \"/\" total[1] \" TB \" int((used[1]/total[1])*100) \"%\"}'"]], 10, function(_, stdout)
-  strg_free_sdb.text = stdout:gsub( "\n", "" )
-end)
+local mem_use = helpers.simplewtch([[sh -c "echo -n 'Used: ' && free -h | grep 'Mem:' | awk '{gsub(/Gi/,\"\",\$3); gsub(/Gi/,\"\",\$2); print \$3\"/\"\$2}' | tr '\n' ' ' && echo 'GiB'"]], 2)
 
-local strg_free_sda = wibox.widget {
-  widget = wibox.widget.textbox,
-  align = "left",
-  valign = "center",
-}
+local cache_use = helpers.simplewtch([[sh -c "echo -n 'Cache: ' && free -h | grep 'Mem:' | awk '{gsub(/Gi/,\"\",\$6); gsub(/Gi/,\"\",\$2); print \$6\"/\"\$2}' | tr '\n' ' ' && echo 'GiB'"]], 5)
 
-awful.widget.watch([[sh -c "echo -n 'SDA: ' && df -h /dev/sda1 | awk 'NR==2 {split(\$3, used, \"G\"); split(\$2, total, \"G\"); print used[1] \"/\" total[1] \" GB \" int((used[1]/total[1])*100) \"%\"}'"]], 10, function(_, stdout)
-  strg_free_sda.text = stdout:gsub( "\n", "" )
-end)
+local network_text = helpers.text("Network", beautiful.font, "center")
 
+local network_total = helpers.simplewtch([[sh -c 'ip -s link show enp9s0 | awk '\''/RX:/{getline; rx=$1} /TX:/{getline; tx=$1} END{printf "Rx/Tx: %sB/%sB\n", convert(rx), convert(tx)} function convert(val) {suffix="BKMGTPE"; for(i=1; val>1024; i++) val/=1024; return int(val+0.5) substr(suffix, i, 1)}'\']], 5)
 
-local cpu_text = wibox.widget {
-  widget = wibox.widget.textbox,
-  text = "CPU",
-  align = "center",
-  valign = "center",
-}
+local gpu_text = helpers.text("GPU", beautiful.font, "center")
 
-local cpu_use = wibox.widget {
-  widget = wibox.widget.textbox,
-  align = "left",
-  valign = "center",
-}
+local gpu_use = helpers.simplewtch([[sh -c "echo -n 'GPU Usage: ' && nvidia-smi | grep 'Default' | cut -d '|' -f 4 | tr -d 'Default' | tr -d '[:space:]'"]], 1)
 
-awful.widget.watch([[sh -c "echo -n 'CPU Usage: ' && top -bn1 | grep '%Cpu' | awk '{print int(100-$8)}' && echo -n '%'"]], 1, function(_, stdout)
-  cpu_use.text = stdout:gsub( "\n", "" )
-end)
+local gpu_temp = helpers.simplewtch([[sh -c "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader | tr -d '\n' && echo 'C'"]], 2)
 
-local cpu_temp = wibox.widget {
-  widget = wibox.widget.textbox,
-  align = "left",
-  valign = "center",
-}
+local gpu_mem = helpers.simplewtch([[sh -c "echo -n 'GPU Mem: ' && nvidia-smi --query-gpu=memory.used,memory.total --format=csv,noheader,nounits | awk -F',' '{printf \"%.2f/%.2f GiB\n\", \$1/1024, \$2/1024}'"]], 2)
 
-awful.widget.watch([[sh -c "cat /sys/class/thermal/thermal_zone*/temp | sed 's/\(.*\)000$/\1/' | tr -d '\n' && echo 'C'"]], 2, function(_, stdout)
-  cpu_temp.text = stdout:gsub( "\n", "" )
-end)
+local strg_text = helpers.text("Storage", beautiful.font, "center")
 
-local mem_text = wibox.widget {
-  widget = wibox.widget.textbox,
-  text = "Memory",
-  align = "center",
-  valign = "center",
-}
+local strg_free_nvme = helpers.simplewtch([[sh -c "echo -n 'NVME: ' && df -h /dev/nvme0n1p3 | awk 'NR==2 {split(\$3, used, \"G\"); split(\$2, total, \"G\"); print used[1] \"/\" total[1] \" GB \" int((used[1]/total[1])*100) \"%\"}'"]], 60)
 
-local mem_use = wibox.widget {
-  widget = wibox.widget.textbox,
-  align = "left",
-  valign = "center",
-}
+local strg_free_sda = helpers.simplewtch([[sh -c "echo -n 'SDA: ' && df -h /dev/sda1 | awk 'NR==2 {split(\$3, used, \"G\"); split(\$2, total, \"G\"); print used[1] \"/\" total[1] \" GB \" int((used[1]/total[1])*100) \"%\"}'"]], 60)
 
-awful.widget.watch([[sh -c "echo -n 'Used: ' && free -h | grep 'Mem:' | awk '{gsub(/Gi/,\"\",\$3); gsub(/Gi/,\"\",\$2); print \$3\"/\"\$2}' | tr '\n' ' ' && echo 'GiB'"]], 2, function(_, stdout)
-  mem_use.text = stdout:gsub( "\n", "" )
-end)
+local strg_free_sdb = helpers.simplewtch([[sh -c "echo -n 'SDB: ' && df -h /dev/sdb1 | awk 'NR==2 {split(\$3, used, \"T\"); split(\$2, total, \"T\"); print used[1] \"/\" total[1] \" TB \" int((used[1]/total[1])*100) \"%\"}'"]], 60)
 
-local cache_use = wibox.widget {
-  widget = wibox.widget.textbox,
-  align = "left",
-  valign = "center",
-}
+local uptime_text = helpers.text("Uptime", beautiful.font, "center")
 
-awful.widget.watch([[sh -c "echo -n 'Cache: ' && free -h | grep 'Mem:' | awk '{gsub(/Gi/,\"\",\$6); gsub(/Gi/,\"\",\$2); print \$6\"/\"\$2}' | tr '\n' ' ' && echo 'GiB'"]], 5, function(_, stdout)
-  cache_use.text = stdout:gsub( "\n", "" )
-end)
-
-local gpu_text = wibox.widget {
-  widget = wibox.widget.textbox,
-  text = "GPU",
-  align = "center",
-  valign = "center",
-}
-
-local gpu_use = wibox.widget {
-  widget = wibox.widget.textbox,
-  align = "left",
-  valign = "center",
-}
-
-awful.widget.watch([[sh -c "echo -n 'GPU Usage: ' && nvidia-smi | grep 'Default' | cut -d '|' -f 4 | tr -d 'Default' | tr -d '[:space:]'"]], 1, function(_, stdout)
-  gpu_use.text = stdout:gsub( "\n", "" )
-end)
-
-local gpu_temp = wibox.widget {
-  widget = wibox.widget.textbox,
-  align = "left",
-  valign = "center",
-}
-
-awful.widget.watch([[sh -c "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader | tr -d '\n' && echo 'C'"]], 2, function(_, stdout)
-  gpu_temp.text = stdout:gsub( "\n", "" )
-end)
-
-local gpu_mem = wibox.widget {
-  widget = wibox.widget.textbox,
-  align = "left",
-  valign = "center",
-}
-
-awful.widget.watch([[sh -c "echo -n 'GPU Mem: ' && nvidia-smi --query-gpu=memory.used,memory.total --format=csv,noheader,nounits | awk -F',' '{printf \"%.2f/%.2f GiB\n\", \$1/1024, \$2/1024}'"]], 2, function(_, stdout)
-  gpu_mem.text = stdout:gsub( "\n", "" )
-end)
-
-local network_text = wibox.widget {
-  widget = wibox.widget.textbox,
-  text = "Network",
-  align = "center",
-  valign = "center",
-}
-
-local network_total = wibox.widget {
-  widget = wibox.widget.textbox,
-  align = "left",
-  valign = "center",
-}
-
-awful.widget.watch([[sh -c 'ip -s link show enp9s0 | awk '\''/RX:/{getline; rx=$1} /TX:/{getline; tx=$1} END{printf "Rx/Tx: %sB/%sB\n", convert(rx), convert(tx)} function convert(val) {suffix="BKMGTPE"; for(i=1; val>1024; i++) val/=1024; return int(val+0.5) substr(suffix, i, 1)}'\']], 5, function(_, stdout)
-  network_total.text = stdout:gsub( "\n", "" )
-end)
-
-local uptime_text = wibox.widget {
-  widget = wibox.widget.textbox,
-  text = "Uptime",
-  align = "center",
-  valign = "center",
-}
-
-local uptime = wibox.widget {
-  widget = wibox.widget.textbox,
-  align = "left",
-  valign = "center",
-}
-
-awful.widget.watch([[sh -c 'uptime | awk -F"[ ,:]+" '\''{print $6 " days, " $8 " hours"}'\''']], 60, function(_, stdout)
-  uptime.text = stdout:gsub( "\n", "" )
-end)
+local uptime = helpers.simplewtch([[sh -c 'uptime | awk -F"[ ,:]+" '\''{print $6 " days, " $8 " hours"}'\''']], 60)
 
 local resourcemenu_container = wibox.widget {
   layout = wibox.layout.align.horizontal,
