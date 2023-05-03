8ea230d3df59ec52212bdcbdde3996bcb3268309
PassiveLemon <lemonl3mn@protonmail.com>
2023-05-02 20:05:00 -0400

    Remove
    Probably wont use

--- .nix/modules/config/awesome/config/helpers/stats.lua
+++
@@ -1,5 +0,0 @@
-
---
--- To eventually hold a centralized list of system statistics like CPU %, Temp, GPU %, Temp, Disk use, Disk space, Network use, etc.
--- Will only show what is updated with no decorations. Ex: Percent signs not included in the output.
---
