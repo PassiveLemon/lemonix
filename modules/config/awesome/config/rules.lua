e715ed38aa0a391853ace3b964af319f3cba30d0
PassiveLemon <lemonl3mn@protonmail.com>
2023-05-30 23:05:08 -0400

    Fix
    Specified floating clients will open in the middle of the screen. The rules will actually be applied when those rules are met.

--- modules/config/awesome/config/rules.lua
+++ modules/config/awesome/config/rules.lua
@@ -19,54 +19,56 @@ ruled.client.connect_signal("request::rules", function()
       placement = awful.placement.no_offscreen
     }
   }
-end)
 
--- Floating clients.
-ruled.client.append_rule {
-  id = "floating",
-  rule_any = {
-    instance = { "feh", "lxappearance", "authy desktop", "xarchiver", "kruler" },
-    class    = { "feh", "Lxappearance", "Authy Desktop", "Xarchiver", "kruler" },
-    name     = { "Customize Look and Feel", "Twilio Authy", "KRuler" },
-    role     = { "pop-up", "GtkFileChooserDialog" },
-  },
-  properties = {
-    floating = true,
-    ontop = true,
-    placement = awful.placement.centered
+-- Always floating clients
+  ruled.client.append_rule {
+    id = "floating",
+    rule_any = {
+      instance = { "feh", "lxappearance", "authy desktop", "xarchiver", "kruler" },
+      class    = { "feh", "Lxappearance", "Authy Desktop", "Xarchiver", "kruler" },
+      name     = { "Customize Look and Feel", "Twilio Authy", "KRuler", "Confirm File Replacing", "Copying files" },
+      role     = { "pop-up", "GtkFileChooserDialog" },
+    },
+    properties = {
+      floating = true,
+      ontop = true,
+      placement = awful.placement.centered+awful.placement.no_offscreen
+    }
   }
-}
+end)
 
 --
 -- Apps
 --
 
-ruled.client.append_rule {
-  rule = {
-    instance = "easyeffects",
-    class    = "easyeffects",
-    name     = "Easy Effects"
-  },
-  properties = { 
-    screen = "DP-0",
-    tag = " 5 ",
-    minimized = true,
-    urgent = false
+ruled.client.connect_signal("request::rules", function()
+  ruled.client.append_rule {
+    rule = {
+      instance = "easyeffects",
+      class    = "easyeffects",
+      name     = "Easy Effects"
+    },
+    properties = { 
+      screen = "DP-0",
+      tag = " 5 ",
+      minimized = true,
+      urgent = false
+    }
   }
-}
 
-ruled.client.append_rule {
-  rule = {
-    instance = "kruler",
-    class    = "kruler",
-    name     = "KRuler"
-  },
-  properties = {
-    width = 1920,
-    height = 75,
-    border_width = 0
+  ruled.client.append_rule {
+    rule = {
+      instance = "kruler",
+      class    = "kruler",
+      name     = "KRuler"
+    },
+    properties = {
+      width = 1920,
+      height = 75,
+      border_width = 0
+    }
   }
-}
+end)
 
 --
 -- Other
