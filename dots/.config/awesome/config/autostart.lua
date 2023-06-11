3f915dada252c21ee56f12d09de571dc5fe1dae6
PassiveLemon <lemonl3mn@protonmail.com>
2023-06-10 22:46:04 -0400

    Change
    More appropriate use for spawning. Also doesn't look like shit

--- dots/.config/awesome/config/autostart.lua
+++ dots/.config/awesome/config/autostart.lua
@@ -2,7 +2,7 @@ local awful = require("awful")
 
 awful.spawn.easy_async("xrandr --output DP-0 --primary --mode 1920x1080 --rate 143.9 --rotate normal --output DP-2 --mode 1920x1080 --rate 143.9 --rotate normal --left-of DP-0")
 
-awful.spawn.easy_async([[sh -c "pgrep easyeffects || easyeffects --gapplication-service"]])
-awful.spawn.easy_async([[sh -c "pgrep picom || picom --experimental-backend -b"]])
-awful.spawn.easy_async([[sh -c "pgrep nm-applet || nm-applet"]])
-awful.spawn.easy_async([[sh -c "pgrep megasync || megasync"]])
+awful.spawn.easy_async_with_shell("pgrep easyeffects || easyeffects --gapplication-service")
+awful.spawn.easy_async_with_shell("pgrep picom || picom --experimental-backend -b")
+awful.spawn.easy_async_with_shell("pgrep nm-applet || nm-applet")
+awful.spawn.easy_async_with_shell("pgrep megasync || megasync")
