c68903baf0b781b4af2a578a37ac336c93bdb7eb
PassiveLemon <lemonl3mn@protonmail.com>
2023-07-16 14:01:57 -0400

    remove redundant popups
    just change the widget of the original popup

--- common/dots/.config/awesome/ui/powermenu.lua
+++ common/dots/.config/awesome/ui/powermenu.lua
@@ -16,84 +16,66 @@ local poweroff = helpers.simplebtn(100, 100, "󰐥", beautiful.font_large, 8, 4,
 
 local restart = helpers.simplebtn(100, 100, "󰑓", beautiful.font_large, 8, 8, 8, 4)
 
-local powermenu_pop = awful.popup {
-  widget = powermenu_container,
-  placement = awful.placement.centered,
-  border_width = 3,
-  border_color = beautiful.border_color_active,
-  ontop = true,
-  visible = false,
-  widget = {
-    layout = wibox.layout.align.vertical,
-    {
-      layout = wibox.layout.fixed.horizontal,
-      lock,
-      poweroff,
-      restart,
-    },
+local powermenu_widget = wibox.widget {
+  layout = wibox.layout.align.vertical,
+  {
+    layout = wibox.layout.fixed.horizontal,
+    lock,
+    poweroff,
+    restart,
   },
 }
 
-local prompt = helpers.simpletxt(300, 36, "Are you sure?", beautiful.font, "center", 8, 8, 4, 8)
+local prompt = helpers.simpletxt(308, 36, "Are you sure?", beautiful.font, "center", 8, 8, 4, 8)
 
 local confirmpow = helpers.simplebtn(100, 56, "Poweroff", beautiful.font, 4, 4, 8, 8)
 
 local confirmres = helpers.simplebtn(100, 56, "Restart", beautiful.font, 4, 4, 8, 8)
 
-local cancel = helpers.simplebtn(200, 56, "Cancel", beautiful.font, 4, 8, 8, 4)
+local cancel = helpers.simplebtn(208, 56, "Cancel", beautiful.font, 4, 8, 8, 4)
 
-local poweroff_pop = awful.popup {
-  widget = poweroff_container,
-  placement = awful.placement.centered,
-  border_width = 3,
-  border_color = beautiful.border_color_active,
-  ontop = true,
-  visible = false,
-  widget = {
-    layout = wibox.layout.align.vertical,
-    {
-      layout = wibox.layout.fixed.horizontal,
-      prompt,
-    },
-    {
-      layout = wibox.layout.fixed.horizontal,
-      confirmpow,
-      cancel,
-    },
+local poweroff_widget = wibox.widget {
+  layout = wibox.layout.align.vertical,
+  {
+    layout = wibox.layout.fixed.horizontal,
+    prompt,
+  },
+  {
+    layout = wibox.layout.fixed.horizontal,
+    confirmpow,
+    cancel,
   },
 }
 
-local restart_pop = awful.popup {
+local restart_widget = wibox.widget {
+  layout = wibox.layout.align.vertical,
+  {
+    layout = wibox.layout.fixed.horizontal,
+    prompt,
+  },
+  {
+    layout = wibox.layout.fixed.horizontal,
+    confirmres,
+    cancel,
+  },
+}
+
+local powermenu_pop = awful.popup {
   placement = awful.placement.centered,
   border_width = 3,
   border_color = beautiful.border_color_active,
   ontop = true,
   visible = false,
-  widget = {
-    layout = wibox.layout.align.vertical,
-    {
-      layout = wibox.layout.fixed.horizontal,
-      prompt,
-    },
-    {
-      layout = wibox.layout.fixed.horizontal,
-      confirmres,
-      cancel,
-    },
-  },
+  widget = powermenu_widget,
 }
 
 local function confirmed(command)
-  poweroff_pop.visible = false
-  restart_pop.visible = false
   powermenu_pop.visible = false
   awful.spawn(command)
 end
 
 cancel:connect_signal("button::press", function()
-  poweroff_pop.visible = false
-  restart_pop.visible = false
-  powermenu_pop.visible = false
+  powermenu_pop.widget = powermenu_widget
 end)
 
 confirmpow:connect_signal("button::press", function()
@@ -110,25 +92,20 @@ lock:connect_signal("button::press", function()
 end)
 
 poweroff:connect_signal("button::press", function()
-  poweroff_pop.visible = true
-  powermenu_pop.visible = false
-  poweroff_pop.screen = awful.screen.focused()
+  powermenu_pop.widget = poweroff_widget
 end)
 
 restart:connect_signal("button::press", function()
-  restart_pop.visible = true
-  powermenu_pop.visible = false
-  restart_pop.screen = awful.screen.focused()
+  powermenu_pop.widget = restart_widget
 end)
 
 local function signal()
+  powermenu_pop.widget = powermenu_widget
   powermenu_pop.visible = not powermenu_pop.visible
   powermenu_pop.screen = awful.screen.focused()
   helpers.unfocus()
 end
 
 click_to_hide.popup(powermenu_pop, nil, true)
-click_to_hide.popup(poweroff_pop, nil, true)
-click_to_hide.popup(restart_pop, nil, true)
 
 return { signal = signal }
