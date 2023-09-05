03e2fb07fa764c084cde5b930696960df46762e0
PassiveLemon <lemonl3mn@protonmail.com>
2023-09-04 20:36:27 -0400

    fix: autostart

--- common/dots/.config/awesome/rc.lua
+++ common/dots/.config/awesome/rc.lua
@@ -4,10 +4,10 @@ local beautiful = require("beautiful")
 beautiful.init("~/.config/awesome/config/theme.lua")
 
 local autostart = os.getenv("HOME") .. "/.config/autostart/"
-awful.spawn.easy_async_with_shell("test -f " .. autostart .. " awesome.sh && echo true || echo false", function(fileTest)
+awful.spawn.easy_async_with_shell("test -f " .. autostart .. "awesome.sh && echo true || echo false", function(fileTest)
   fileTest = fileTest.gsub(fileTest, "\n", "")
   if fileTest == "true" then
-    awful.spawn.easy_async_with_shell(autostart .. "awesome.sh")
+    awful.spawn.easy_async_with_shell("sh " .. autostart .. "awesome.sh")
   end
 end)
 
