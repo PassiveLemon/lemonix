0c55c0323074ed09847638564d864708d96544e9
PassiveLemon <lemonl3mn@protonmail.com>
2023-12-01 16:38:59 -0500

    Remove: r2modman
    Its updated in Nixpkgs master

--- common/modules/gaming.nix
+++ common/modules/gaming.nix
@@ -1,7 +1,7 @@
 { inputs, outputs, pkgs, config, lib, ... }: {
   home.packages = with pkgs; [
     steam
-    (callPackage ../../pkgs/r2modman { })
+    master.r2modman
     lunar-client prismlauncher #(callPackage ../../pkgs/gdlauncher-carbon { })
     (callPackage ../../pkgs/vinegar { })
     yuzu-mainline
