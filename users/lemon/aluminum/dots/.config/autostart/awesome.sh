754d360c0bf28a7ca81e62137656eef91025a309
PassiveLemon <lemonl3mn@protonmail.com>
2023-12-22 21:44:24 -0500

    Refactor: Process background stuff before display stuff
    Hopefully gives time for X to fully set up so when the xrandr commands are run, they actually work.

--- users/lemon/aluminum/dots/.config/autostart/awesome.sh
+++ users/lemon/aluminum/dots/.config/autostart/awesome.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+# Other
+mkdir -p $HOME/.cache/lemonix/mediamenu/
+
 # Display
+sleep 0.5
 xrandr --output eDP-1 --primary --mode 1920x1080 --rate 60.01 --rotate normal
 xrandr --output eDP-1 --gamma 1.0:0.92:0.92
 
 # Programs
 pgrep -x nm-applet > /dev/null || nm-applet &
 pgrep -x blueman-applet > /dev/null || blueman-applet &
-
-# Other
-mkdir -p $HOME/.cache/lemonix/mediamenu/
--- users/lemon/silver/dots/.config/autostart/awesome.sh
+++ users/lemon/silver/dots/.config/autostart/awesome.sh
@@ -3,19 +3,20 @@
 # Killall
 killall -q picom
 
-# Display
-xrandr --output DP-0 --primary --mode 1920x1080 --rate 143.9 --rotate normal --output DP-2 --mode 1920x1080 --rate 143.9 --rotate normal --left-of DP-0
-xrandr --output DP-0 --gamma 1.0:0.92:0.92 --output DP-2 --gamma 1.0:0.92:0.92
-
 # Hardware
 headsetcontrol -l 0
-headsetcontrol -s 100
+headsetcontrol -s 110
+
+# Other
+mkdir -p $HOME/.cache/lemonix/mediamenu/
+
+# Display
+sleep 0.5
+xrandr --output DP-0 --primary --mode 1920x1080 --rate 143.9 --rotate normal --output DP-2 --mode 1920x1080 --rate 143.9 --rotate normal --left-of DP-0
+xrandr --output DP-0 --gamma 1.0:0.92:0.92 --output DP-2 --gamma 1.0:0.92:0.92
 
 # Programs
 picom -b &
 pgrep -x easyeffects > /dev/null || easyeffects --gapplication-service &
 pgrep -x nm-applet > /dev/null || nm-applet &
 pgrep -x megasync > /dev/null || megasync &
-
-# Other
-mkdir -p $HOME/.cache/lemonix/mediamenu/
