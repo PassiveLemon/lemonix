604afd27b831027b9c59e3fcbb82bb020bcecc3a
PassiveLemon <lemonl3mn@protonmail.com>
2024-02-07 21:13:03 -0500

    Remove: Lemonscripts
    xdg-open doesn't like running the symlinked scripts

--- common/dots/.config/lemonscripts/Auto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /home/docker/unstable-diffusion-webui-docker
-echo "Killing webui-docker-auto-1"
-docker stop webui-docker-auto-1
-echo "Building webui-docker-auto-1"
-docker compose --profile auto up --build
--- common/dots/.config/lemonscripts/ComfyUI.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /home/docker/unstable-diffusion-webui-docker
-echo "Killing webui-docker-comfy-1"
-docker stop webui-docker-comfy-1
-echo "Building webui-docker-comfy-1"
-docker compose --profile comfy up --build
