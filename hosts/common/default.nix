d2a72c453f783388afa23f3b499ab4a7823c8905
PassiveLemon <lemonl3mn@protonmail.com>
2024-02-18 11:16:18 -0500

    Change: GC

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -78,7 +78,7 @@
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 30d";
+      options = "--delete-older-than 14d";
     };
   };
   nixpkgs.config = {
