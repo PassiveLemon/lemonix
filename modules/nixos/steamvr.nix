a31cab6461483451a0ae74a85583b68a37e5f1d1
PassiveLemon <lemonl3mn@protonmail.com>
2024-03-02 21:59:11 -0500

    Feature: Monado for SteamVR
    Haven't actually tested it yet but I'm committing it anyways.
    It's a local import until it gets ported to 23.11 stable.

--- modules/nixos/steamvr.nix
+++ modules/nixos/steamvr.nix
@@ -1,14 +1,22 @@
 { inputs, pkgs, config, lib, ... }: {
   imports = [
     inputs.lemonake.nixosModules.alvr
+    ./monado.nix
   ];
 
   environment.systemPackages = with pkgs; [
-    BeatSaberModManager autoadb
+    monado autoadb BeatSaberModManager
   ];
-  services.udev.packages = [
-    pkgs.android-udev-rules
-  ];
+  services = {
+    udev.packages = [
+      pkgs.android-udev-rules
+    ];
+    monado = { # Local import until it gets ported to 23.11 stable
+      enable = true;
+      highPriority = true;
+      defaultRuntime = true;
+    };
+  };
   programs = {
     adb.enable = true;
     alvr = { # Module of lemonake
