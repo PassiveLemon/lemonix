6387b440c6f4a9e69b0b77d0900a5af87c5b74c7
PassiveLemon <lemonl3mn@protonmail.com>
2024-03-02 21:58:21 -0500

    Fix: Disable nix-gaming pipewire
    Disabled until upstream is fixed.

--- hosts/silver/user.nix
+++ hosts/silver/user.nix
@@ -2,20 +2,20 @@
   imports = [
     ../common/user.nix
     ../../modules/nixos/bluetooth.nix
-    inputs.nix-gaming.nixosModules.pipewireLowLatency
+    #inputs.nix-gaming.nixosModules.pipewireLowLatency
     inputs.nix-gaming.nixosModules.steamCompat
     ../../modules/nixos/steamvr.nix
   ];
 
   # Configs
   services = {
-    pipewire = {
-      lowLatency = { # Module of nix-gaming
-        enable = true;
-        quantum = 128;
-        rate = 48000;
-      };
-    };
+    #pipewire = {
+    #  lowLatency = { # Module of nix-gaming
+    #    enable = true;
+    #    quantum = 128;
+    #    rate = 48000;
+    #  };
+    #};
     udev.extraRules = ''
       ACTION!="add|change", GOTO="headset_end"
       KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="0a51", TAG+="uaccess"
