00b7143bbbe3369e66b75aa6c6dd86ab1b1c4c88
PassiveLemon <lemonl3mn@protonmail.com>
2024-04-15 12:18:24 -0400

    Refactor: Autorandr. No more jank.

--- users/lemon/aluminum/home.nix
+++ users/lemon/aluminum/home.nix
@@ -11,23 +11,54 @@
     ];
     file = {
       ".config/autostart/" = {
-        source = ./dots/.config/autostart;
+        source = ./home/.config/autostart;
         recursive = true;
       };
       ".config/awesome/config" = {
-        source = ./dots/.config/awesome/config;
+        source = ./home/.config/awesome/config;
         recursive = true;
       };
       ".config/awesome/signal" = {
-        source = ./dots/.config/awesome/signal;
+        source = ./home/.config/awesome/signal;
         recursive = true;
       };
       ".config/awesome/ui" = {
-        source = ./dots/.config/awesome/ui;
+        source = ./home/.config/awesome/ui;
         recursive = true;
       };
     };
     stateVersion = "23.05";
+  };
+
+  services = {
+    autorandr.enable = true;
+  };
+
+  programs = {
+    autorandr = {
+      enable = true;
+      profiles = {
+        "Default" = {
+          fingerprint = {
+            eDP-1 = "xxxxxx";
+          };
+          config = {
+            "eDP-1" = {
+              enable = true;
+              primary = true;
+              position = "0x0";
+              mode = "1920x1080";
+              rate = "60.01";
+              gamma = "1.0:0.92:0.92";
+            };
+          };
+        };
+      };
+    };
+  };
+
+  xsession = {
+    windowManager.awesome.noArgb = true;
   };
 
   nixpkgs = {
--- users/lemon/silver/home/.config/autostart/awesome.sh
+++ users/lemon/silver/home/.config/autostart/awesome.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-autorandr Default
-
 # Hardware
 headsetcontrol -l 0 -s 110
 
@@ -10,9 +8,5 @@ pgrep -x picom > /dev/null || picom -b &
 pgrep -x easyeffects > /dev/null || easyeffects --gapplication-service &
 pgrep -x nm-applet > /dev/null || nm-applet &
 pgrep -x megasync > /dev/null || megasync &
-
-# Delay start to hopefully ensure it gets started
-sleep 0.5
-autorandr Default
 
 
