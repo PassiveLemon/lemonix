bed0fddb5074d742c2ea3abbb24b3d05365f3a3a
PassiveLemon <lemonl3mn@protonmail.com>
2024-05-27 17:24:27 -0400

    Update: OpenComposite
    Just using Scrums package since it builds now.

--- pkgs/opencomposite/default.nix
+++ pkgs/opencomposite/default.nix
@@ -1,8 +1,9 @@
 { lib
 , stdenv
 , fetchFromGitLab
-, fetchpatch
+
 , cmake
+
 , glm
 , libGL
 , openxr-loader
@@ -10,31 +11,25 @@
 , vulkan-headers
 , vulkan-loader
 , xorg
+
+, unstableGitUpdater
 }:
+
 stdenv.mkDerivation {
   pname = "opencomposite";
-  version = "unstable-2024-05-13";
+  version = "0-unstable-2024-05-24";
 
   src = fetchFromGitLab {
     owner = "znixian";
     repo = "OpenOVR";
-    rev = "5ddd6024efafa82c7a432c9dd8a67e3d5c3f9b38";
-    hash = "sha256-HgbQJl2qo1TAlq84UfJ9JvA5eTVIeRRyvaJeBxQGZAs=";
-    #fetchSubmodules = true;
+    rev = "762f93d91f4c23ad70c81c81486b6bcd7e9bbb5e";
+    hash = "sha256-Z1Is+yjyAG8X5+FWaxtCkF7paRGV9ZlNVubuVkeO7yg=";
   };
-
-  # A commit (1edb2f37) causes problems when building so I pull the commit before it (5ddd6024) and just patch in the commit after it (bc79a84d)
-  # The problematic commit does not appear very useful to regular users
-  patches = [
-    (fetchpatch {
-      url = "https://gitlab.com/znixian/OpenOVR/-/commit/bc79a84d69b1c5a84db3f79c9a0c75278a7aea6f.patch";
-      hash = "sha256-oz4z2YCTWrAD86eod2ETL1bDhJcH3mAOixIIb0ce+nM=";
-    })
-  ];
 
   nativeBuildInputs = [
     cmake
   ];
+
   buildInputs = [
     glm
     libGL
@@ -48,22 +43,26 @@ stdenv.mkDerivation {
   cmakeFlags = [
     (lib.cmakeBool "USE_SYSTEM_OPENXR" true)
     (lib.cmakeBool "USE_SYSTEM_GLM" true)
+    # debug logging macros cause format-security warnings
+    (lib.cmakeFeature "CMAKE_CXX_FLAGS" "-Wno-error=format-security")
   ];
 
   installPhase = ''
     runHook preInstall
-
     mkdir -p $out/lib/opencomposite
     cp -r bin/ $out/lib/opencomposite
-
     runHook postInstall
   '';
+
+  passthru.updateScript = unstableGitUpdater {
+    hardcodeZeroVersion = true;
+    branch = "openxr";
+  };
 
   meta = with lib; {
     description = "Reimplementation of OpenVR, translating calls to OpenXR";
     homepage = "https://gitlab.com/znixian/OpenOVR";
-    license = licenses.gpl3Only;
-    maintainers = with maintainers; [ passivelemon ];
-    platforms = platforms.linux;
+    license = with licenses; [ gpl3Only ];
+    maintainers = with maintainers; [ Scrumplex ];
   };
 }
