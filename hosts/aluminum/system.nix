1c71c7818bf4eb308644ff617d3ccc54c727a0b7
PassiveLemon <lemonl3mn@protonmail.com>
2024-07-09 21:46:40 -0400

    Update: Power management
    Still a test but I think there is a noticeable improvement.

--- hosts/aluminum/system.nix
+++ hosts/aluminum/system.nix
@@ -49,6 +49,28 @@
     fwupd.enable = true;
     thermald.enable = true;
     upower.enable = true;
+    power-profiles-daemon.enable = lib.mkForce false;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "low-power";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 80;
+
+        START_CHARGE_THRESH_BAT0 = 70; # 60 and below it starts to charge
+        STOP_CHARGE_THRESH_BAT0 = 90;
+      };
+    };
   };
 
   powerManagement = {
