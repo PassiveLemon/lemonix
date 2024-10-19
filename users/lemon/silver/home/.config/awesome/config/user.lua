aee0a437ded78d96a19ec4ae0c425886457ac13a
PassiveLemon <lemonl3mn@protonmail.com>
2024-10-18 22:01:37 -0400

    Feature: Lock AWM on sleep

--- users/lemon/silver/home/.config/awesome/config/user.lua
+++ users/lemon/silver/home/.config/awesome/config/user.lua
@@ -11,6 +11,7 @@ user.super = "Mod4" -- Windows key
 user.has_brightness = false
 
 awful.spawn.with_shell("autorandr -l Default")
+awful.spawn.with_shell("pidof -q xss-lock || xss-lock awesome-client 'awesome.emit_signal(\"ui::lock::toggle\")' &")
 awful.spawn.with_shell("pidof -q picom || picom --realtime -b")
 awful.spawn.with_shell("pidof -q easyeffects || easyeffects --gapplication-service")
 awful.spawn.with_shell("pidof -q nm-applet || nm-applet")
