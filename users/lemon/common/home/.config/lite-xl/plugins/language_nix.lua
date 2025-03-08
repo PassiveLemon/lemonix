fff2a12b7e1c010323c4b3b44d9c7de747cb65d0
PassiveLemon <lemonl3mn@protonmail.com>
2025-03-08 12:44:14 -0500

    fix: some lite-xl nix patterns.

--- users/lemon/common/home/.config/lite-xl/plugins/language_nix.lua
+++ users/lemon/common/home/.config/lite-xl/plugins/language_nix.lua
@@ -9,7 +9,7 @@ local function merge_tables(a, b)
 end
 
 local default_symbols = {
-  ["import"]   = "operator",
+  ["import"]   = "keyword",
   ["with"]     = "keyword",
   ["inherit"]  = "keyword",
   ["assert"]   = "keyword",
@@ -28,12 +28,29 @@ local default_symbols = {
 local default_patterns = { }
 
 local string_interpolation = {
+  -- Escapement
+  { pattern = { "''%${", "}" }, type = "keyword", syntax = {
+    patterns = default_patterns,
+    symbols = default_symbols,
+  }},
+
   { pattern = { "%${", "}" }, type = "keyword", syntax = {
     patterns = default_patterns,
     symbols = default_symbols,
   }},
+
   { pattern = "[%S][%w]*", type = "string" },
 }
+
+-- local brace_interpolation = {
+--   { pattern = "%S+%s*,%s", type = "normal" },
+--   { pattern = ",%s*%S+",   type = "normal" },
+--   { pattern = "%s*%S+%s*%?",   type = "normal" },
+-- }
+
+-- local list_interpolation = {
+--   { pattern = "%s*%S+%s*%?",   type = "normal" },
+-- }
 
 merge_tables(default_patterns, {
   -- Comments
@@ -41,7 +58,7 @@ merge_tables(default_patterns, {
   { pattern = { "/%*", "%*/" }, type = "comment" },
 
   -- Numeric
-  { pattern = "-?%.?%d+",       type = "number" },
+  { pattern = "-?%.?%d+", type = "number" },
 
   -- Interpolation
   { pattern = { "%${", "}" }, type = "keyword", syntax = {
@@ -52,10 +69,20 @@ merge_tables(default_patterns, {
     patterns = string_interpolation,
     symbols = { },
   }},
-  { pattern = { "''", "''" }, type = "string", syntax = {
+  --                      v We need this to handle escapements, but in return, it grabs our semi-colon
+  { pattern = { "''", "''[^%$]" }, type = "string", syntax = {
     patterns = string_interpolation,
     symbols = { },
   }},
+  -- Some ideas to handle module attributes. Need some heavy refinement
+  -- { pattern = { "{", "}:%S" }, type = "normal", syntax = {
+  --   patterns = brace_interpolation,
+  --   symbols = default_symbols,
+  -- }},
+  -- { pattern = { "%s", ",%s" }, type = "normal", syntax = {
+  --   patterns = list_interpolation,
+  --   symbols = default_symbols,
+  -- }},
 
   -- Operators
   { pattern = "[%+%-%?!>%*]", type = "normal" },
@@ -75,30 +102,57 @@ merge_tables(default_patterns, {
   { pattern = "~/[^%s%[%]%(%){};,:]+",      type = "string" },
   { pattern = { "<", ">" },                 type = "string" },
 
+  -- Functions
+  { pattern = "%S+%s*:%s", type = "normal" },
+
+  -- Module attributes
+  { pattern = "%S+%s*,%s", type = "normal" },
+  { pattern = ",%s*%S+",   type = "normal" },
+
   -- Other patterns
-  { -- Match variables/attribute keys
-    pattern = "%s*()[%w%-%_%.]*()%s*()=()%s*",
-    type = { "normal", "literal", "normal", "normal", "normal" }
+  -- Variables/attributes with a strings
+  {
+    pattern = '%s*()".*"()[%w%-%_%.]*()".*"()%s*()=()%s*',
+    type = { "normal", "string", "literal", "string", "normal", "normal" },
   },
-  { -- Match inherits with a namespace
+  {
+    pattern = '%s*()[%w%-%_%.]*()".*"()[%w%-%_%.]*()%s*()=()%s*',
+    type = { "normal", "literal", "string", "literal", "normal", "normal" },
+  },
+  {
+    pattern = '%s*()[%w%-%_%.]*()".*"()%s*()=()%s*',
+    type = { "normal", "literal", "string", "normal", "normal" },
+  },
+  {
+    pattern = '%s*()".*"()[%w%-%_%.]*()%s*()=()%s*',
+    type = { "normal", "string", "literal", "normal", "normal" },
+  },
+
+  -- Variables/attributes
+  {
+    pattern = "%s*()[%w%-%_%.]*()%s*()=()%s*",
+    type = { "normal", "literal", "normal", "normal", "normal" },
+  },
+
+  -- Inherits
+  {
     pattern = "inherit()%s*%(()%w+()%)()%s*.-%s*();",
     type = { "keyword", "normal", "keyword2", "normal", "literal", "normal" },
   },
-  { -- Match inherits
+  {
     pattern = "inherit()%s*.-%s*();",
     type = { "keyword", "literal", "normal" },
   },
-  { pattern = "%S+%s*[,:]%s",      type = "normal" },
+
   -- Everything else
   { pattern = "[%a%-%_][%w%-%_]*", type = "keyword2" },
 })
 
--- Problems:
--- Attribute assignment dots are the same color
--- Quoted strings in an in-line attribute assignment are the same color
--- Multi-line file function arguments are a different color
-
--- Some way to represent repeated value.value, like ([%w].[%w])+
+-- Current issues:
+-- Attribute dots are the same color. Not a big deal. Ex: programs.firefox.profiles.etc
+--                                 Should be gray, but are orange ^       ^        ^
+-- Escaped nix expressions in strings result in the following semi-colon turning green.
+-- A variety of issues regarding module attributes.
 
 syntax.add({
   name = "Nix",
