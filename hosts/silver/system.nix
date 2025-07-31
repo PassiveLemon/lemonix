bbaabd175ba0b0b4c80ac7b090e9b60de6c79f5f
PassiveLemon <lemonl3mn@protonmail.com>
2025-07-30 23:02:32 -0400

    fix: silver nvidia package
    thanks for breaking stuff all the time

--- hosts/silver/system.nix
+++ hosts/silver/system.nix
@@ -11,7 +11,7 @@
     };
     kernelModules = [ "iwlwifi" "kvm-amd" ];
     # https://github.com/NixOS/nixpkgs/issues/412299
-    kernelPackages = pkgs.linuxPackages_6_14;
+    kernelPackages = pkgs.linuxPackages_6_15;
   };
 
   networking = {
@@ -112,9 +112,27 @@
     libvirtd.enable = true;
   };
 
-  hardware = {
+  hardware = let
+    gpl_symbols_linux_615_patch = pkgs.fetchpatch {
+      url = "https://github.com/CachyOS/kernel-patches/raw/914aea4298e3744beddad09f3d2773d71839b182/6.15/misc/nvidia/0003-Workaround-nv_vm_flags_-calling-GPL-only-code.patch";
+      hash = "sha256-YOTAvONchPPSVDP9eJ9236pAPtxYK5nAePNtm2dlvb4=";
+      stripLen = 1;
+      extraPrefix = "kernel/";
+    };
+    nvidia-fix = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "575.57.08";
+      openSha256 = "sha256-DOJw73sjhQoy+5R0GHGnUddE6xaXb/z/Ihq3BKBf+lg=";
+      sha256_64bit = "sha256-KqcB2sGAp7IKbleMzNkB3tjUTlfWBYDwj50o3R//xvI=";
+      settingsSha256 = "sha256-AIeeDXFEo9VEKCgXnY3QvrW5iWZeIVg4LBCeRtMs5Io=";
+      persistencedSha256 = "sha256-Len7Va4HYp5r3wMpAhL4VsPu5S0JOshPFywbO7vYnGo=";
+      usePersistenced = true;
+      patches = [ gpl_symbols_linux_615_patch ];
+    };
+  in {
     nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      # https://github.com/NixOS/nixpkgs/issues/412299
+      # package = config.boot.kernelPackages.nvidiaPackages.beta;
+      package = nvidia-fix;
       open = true;
       modesetting.enable = true;
       powerManagement.enable = true;
