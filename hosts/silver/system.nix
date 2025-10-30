71d7d9289aead3d8b092a26c92e688aa7c8f0590
PassiveLemon <lemonl3mn@protonmail.com>
2025-10-29 21:17:57 -0400

    fix: v4l2loopback for virtual camera

--- hosts/silver/system.nix
+++ hosts/silver/system.nix
@@ -9,7 +9,13 @@
       grub.enable = false;
       systemd-boot.enable = true;
     };
-    kernelModules = [ "iwlwifi" "kvm-amd" ];
+    kernelModules = [ "iwlwifi" "kvm-amd" "v4l2loopback" ];
+    extraModulePackages = [
+      config.boot.kernelPackages.v4l2loopback.out
+    ];
+    extraModprobeConfig = ''
+      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+    '';
   };
 
   networking = {
