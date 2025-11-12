322efa0c4f182f739e303ee2966c724919fe6e48
PassiveLemon <lemonl3mn@protonmail.com>
2025-11-11 21:49:52 -0500

    chore: disable titanium nfs
    ill just enable it when i momentarily need it

--- hosts/silver/system.nix
+++ hosts/silver/system.nix
@@ -139,11 +139,12 @@
       options = [ "noatime" ];
     };
     # Titanium NFS
-    "/mnt/titanium" = {
-      device = "titanium:/";
-      fsType = "nfs";
-      options = [ "noauto" "x-systemd.automount" "x-systemd.idle-timeout=600" ];
-    };
+    # Not super reliable
+    # "/mnt/titanium" = {
+    #   device = "titanium:/";
+    #   fsType = "nfs";
+    #   options = [ "noauto" "x-systemd.automount" "x-systemd.idle-timeout=600" ];
+    # };
   };
 
   system.stateVersion = "23.05"; # Don't change unless you know what you are doing
