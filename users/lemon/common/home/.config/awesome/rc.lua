bf37c484a2fa37848c4c8511bfc2c63f1030f3d0
PassiveLemon <lemonl3mn@protonmail.com>
2025-11-19 14:25:37 -0500

    fix: awm config
    i guess it took a while for the problematic glib version to make its way to my config. that was a bitch to debug

--- users/lemon/common/home/.config/awesome/rc.lua
+++ users/lemon/common/home/.config/awesome/rc.lua
@@ -1,3 +1,19 @@
+-- https://github.com/awesomeWM/awesome/issues/4029
+-- The cause of the above issue broke the bling app_launcher without causing
+-- an error message. Will fix my bling repo at some point.
+
+local lgi = require("lgi")
+local Gio = lgi.Gio
+local GioUnix = lgi.GioUnix
+
+if not Gio.UnixInputStream and GioUnix then
+  Gio.UnixInputStream = GioUnix.InputStream
+  Gio.UnixOutputStream = GioUnix.OutputStream
+end
+if not Gio.DesktopAppInfo and GioUnix then
+  Gio.DesktopAppInfo = GioUnix.DesktopAppInfo
+end
+
 require("config")
 require("ui")
 
