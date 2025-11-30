6e306a29d47d882e6e1dee8c65b8dc80699f036a
PassiveLemon <lemonl3mn@protonmail.com>
2025-11-29 19:19:39 -0500

    fix: add the awm compatibility cases back
    not really sure why its a problem AGAIN but whatever

--- users/lemon/common/home/.config/awesome/rc.lua
+++ users/lemon/common/home/.config/awesome/rc.lua
@@ -1,3 +1,15 @@
+local lgi = require("lgi")
+local Gio = lgi.Gio
+local GioUnix = lgi.GioUnix
+
+if not Gio.UnixInputStream and GioUnix then
+  Gio.UnixInputStream = GioUnix.InputStream
+  Gio.UnixOutputStream = GioUnix.OutputStream
+end
+if not Gio.DesktopAppInfo and GioUnix then
+  Gio.DesktopAppInfo = GioUnix.DesktopAppInfo
+end
+
 require("config")
 require("ui")
 
