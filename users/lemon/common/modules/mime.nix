3274d650aa2ef20adcdc645726b884c52e1c2e80
PassiveLemon <lemonl3mn@protonmail.com>
2026-01-10 14:10:16 -0500

    refactor: mime app gen function

--- users/lemon/common/modules/mime.nix
+++ users/lemon/common/modules/mime.nix
@@ -1,51 +1,65 @@
+{ lib, ... }:
+let
+  inherit (lib) genAttrs mergeAttrsList;
+  genMimeApps = app: types: (genAttrs types (name: app));
+    
+in
 {
   xdg = {
     mime.enable = true;
     mimeApps = {
       enable = true;
-      defaultApplications = {
-        "application/octet-stream" = "org.lite_xl.lite_xl.desktop";
-        "application/pdf" = "org.gnome.Papers.desktop";
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "onlyoffice-desktopeditors.desktop";
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = "onlyoffice-desktopeditors.desktop";
-        "application/x-extension-htm" = "firefox.desktop";
-        "application/x-extension-html" = "firefox.desktop";
-        "application/x-extension-shtml" = "firefox.desktop";
-        "application/x-extension-xht" = "firefox.desktop";
-        "application/x-extension-xhtml" = "firefox.desktop";
-        "application/xhtml+xml" = "firefox.desktop";
-        "application/zip" = "xarchiver.desktop";
-        "audio/flac" = "mpv.desktop";
-        "audio/matroska" = "mpv.desktop";
-        "audio/mpeg" = "mpv.desktop";
-        "audio/ogg" = "mpv.desktop";
-        "audio/opus" = "mpv.desktop";
-        "audio/vorbis" = "mpv.desktop";
-        "audio/wav" = "mpv.desktop";
-        "audio/x-opus+ogg" = "mpv.desktop";
-        "image/bmp" = "org.gnome.Loupe.desktop";
-        "image/gif" = "org.gnome.Loupe.desktop";
-        "image/heic" = "org.gnome.Loupe.desktop";
-        "image/heif" = "org.gnome.Loupe.desktop";
-        "image/jpeg" = "org.gnome.Loupe.desktop";
-        "image/png" = "org.gnome.Loupe.desktop";
-        "image/svg+xml" = "org.gnome.Loupe.desktop";
-        "image/webp" = "org.gnome.Loupe.desktop";
-        "inode/directory" = "pcmanfm.desktop";
-        "text/html" = "firefox.desktop";
-        "text/plain" = "org.lite_xl.lite_xl.desktop";
-        "video/matroska" = "mpv.desktop";
-        "video/mp4" = "mpv.desktop";
-        "video/mpeg" = "mpv.desktop";
-        "video/MPV" = "mpv.desktop";
-        "video/ogg" = "mpv.desktop";
-        "video/quicktime" = "mpv.desktop";
-        "video/webm" = "mpv.desktop";
-        "video/x-matroska" = "mpv.desktop";
-        "x-scheme-handler/chrome" = "firefox.desktop";
-        "x-scheme-handler/http" = "firefox.desktop";
-        "x-scheme-handler/https" = "firefox.desktop";
-      };
+      defaultApplications = mergeAttrsList [
+        (genMimeApps "firefox.desktop" [
+          "application/x-extension-htm"
+          "application/x-extension-html"
+          "application/x-extension-shtml"
+          "application/x-extension-xht"
+          "application/x-extension-xhtml"
+          "application/xhtml+xml"
+          "text/html"
+          "x-scheme-handler/chrome"
+          "x-scheme-handler/http"
+          "x-scheme-handler/https"
+        ])
+        (genMimeApps "org.gnome.Loupe.desktop" [
+          "image/bmp"
+          "image/gif"
+          "image/heic"
+          "image/heif"
+          "image/jpeg"
+          "image/png"
+          "image/svg+xml"
+          "image/webp"
+        ])
+        (genMimeApps "mpv.desktop" [
+          "audio/flac"
+          "audio/matroska"
+          "audio/mpeg"
+          "audio/ogg"
+          "audio/opus"
+          "audio/vorbis"
+          "audio/wav"
+          "audio/x-opus+ogg"
+          "video/matroska"
+          "video/mp4"
+          "video/mpeg"
+          "video/MPV"
+          "video/ogg"
+          "video/quicktime"
+          "video/webm"
+          "video/x-matroska"
+        ])
+        {
+          "application/octet-stream" = "org.lite_xl.lite_xl.desktop";
+          "application/pdf" = "org.gnome.Papers.desktop";
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "onlyoffice-desktopeditors.desktop";
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = "onlyoffice-desktopeditors.desktop";
+          "application/zip" = "xarchiver.desktop";
+          "inode/directory" = "pcmanfm.desktop";
+          "text/plain" = "org.lite_xl.lite_xl.desktop";
+        }
+      ];
     };
     configFile = {
       "mimeapps.list".force = true;
