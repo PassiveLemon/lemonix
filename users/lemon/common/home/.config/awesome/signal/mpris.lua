7658f041b3cf5384f3961b0aa2e0728630dbb0b6
PassiveLemon <lemonl3mn@protonmail.com>
2026-01-10 21:32:03 -0500

    refactor: mpris awm signal

--- users/lemon/common/home/.config/awesome/signal/mpris.lua
+++ users/lemon/common/home/.config/awesome/signal/mpris.lua
@@ -13,26 +13,20 @@ local mpris = require("lgi").require("AstalMpris")
 --   }
 -- }
 
-local metadata = {
-  ["global"] = {
-    player = {
-      available = false,
-      status = "",
-    },
-  }
-}
+local metadata = { }
+
+-- players = {
+--   ["player"] = mpris.Player
+-- }
+
+local players = { }
 
 local function emit()
   awesome.emit_signal("signal::mpris::metadata", metadata)
 end
 
--- players = {
---   [(player)] = mpris.Player
--- }
-
-local players = { }
-
 local function init_players()
+  local i = 0
   for _, player in ipairs(b.mpris_players) do
     players[string.lower(player)] = mpris.Player.new(player)
     metadata[string.lower(player)] = {
@@ -54,16 +48,17 @@ local function init_players()
         volume = "",
       },
     }
+    -- Temporary, set the global player to the first player
+    if i == 0 then
+      players["global"] = string.lower(player)
+      i = 1
+    end
   end
 end
 
 init_players()
 
--- Temporary
-local global_player = "feishin"
-
-local function art_image_locator(cache_dir, trim, player)
-  local player_metadata = metadata[player]
+local function art_image_locator(cache_dir, trim, pm)
   local art_cache_dir = b.mpris_art_cache_dir
   -- We set a fallback directory if user does not define one
   if not art_cache_dir then
@@ -72,16 +67,16 @@ local function art_image_locator(cache_dir, trim, player)
   if not cache_dir then
     local art_path = h.join_path(art_cache_dir, trim)
     if h.is_file(art_path) then
-      player_metadata.media.art_image = gears.surface.load_uncached(art_path)
+      pm.media.art_image = gears.surface.load_uncached(art_path)
     else
-      awful.spawn.easy_async_with_shell("curl -Lso " .. art_path .. ' "' .. player_metadata.media.art_url .. '"', function()
-        player_metadata.media.art_image = gears.surface.load_uncached(art_path)
+      awful.spawn.easy_async_with_shell("curl -Lso " .. art_path .. ' "' .. pm.media.art_url .. '"', function()
+        pm.media.art_image = gears.surface.load_uncached(art_path)
       end)
     end
   else
     local player_art_path = h.join_path(cache_dir, trim)
     if h.is_file(player_art_path) then
-      player_metadata.media.art_image = gears.surface.load_uncached(player_art_path)
+      pm.media.art_image = gears.surface.load_uncached(player_art_path)
     end
   end
 end
@@ -112,26 +107,24 @@ local art_image_player_lookup = {
   },
 }
 
-local function art_image_fetch(player)
-  local player_metadata = metadata[player]
+local function art_image_fetch(pm)
   -- We normalize the album name and use that as the cache name for the album art, that way it's only downloaded once per album, which makes caching more efficient. In case the normalization results in a bad filename, we use a backup string
-  local player_lookup = art_image_player_lookup[string.lower(player_metadata.player.name)]
+  local player_lookup = art_image_player_lookup[string.lower(pm.player.name)]
   local trim = ""
   if not player_lookup.trim then
-    local album_string = player_metadata.media.album:gsub("%W", "")
+    local album_string = pm.media.album:gsub("%W", "")
     if album_string == "" or not album_string then
-      trim = player_metadata.media.art_url:match(player_lookup.backup)
+      trim = pm.media.art_url:match(player_lookup.backup)
     else
       trim = album_string
     end
   end
-  art_image_locator(player_lookup.cache, trim, player)
+  art_image_locator(player_lookup.cache, trim, pm)
 end
 
 -- If the art isn't already cached then the notification will have the art of the previous media
-local function track_notification(player)
-  local player_metadata = metadata[player]
-  local p_name = string.lower(player_metadata.player.name)
+local function track_notification(pm)
+  local p_name = pm.player.name
   -- Don't show a notification if the music player is visible
   for s in screen do
     for _, c in pairs(s.clients) do
@@ -142,53 +135,50 @@ local function track_notification(player)
       end
     end
   end
-  if b.mpris_notifications and player_metadata.player.available then
+  if b.mpris_notifications and pm.player.available then
     awesome.emit_signal("ui::control::notification::mpris")
   end
 end
 
--- If no overrides, set the global player to the same metadata?
-
 local function metadata_fetch()
-  for player_name, player in pairs(players) do
-    local metadata_player = metadata[string.lower(player_name)]
-    if player.available then
+  for p_name, p in pairs(players) do
+    local pm = metadata[p_name]
+    if p.available then
       -- Sleep here to "improve" responsiveness. Otherwise it happens so quickly that we end up grabbing the old media metadata
       awful.spawn.easy_async("sleep 0.1", function()
         -- TODO: Find a nicer way to do this.
-        local old_media = metadata_player.media.art_url..metadata_player.media.title..metadata_player.media.artist..metadata_player.media.album..metadata_player.media.length
+        local old_media = pm.media.art_url..pm.media.title..pm.media.artist..pm.media.album..pm.media.length
         -- Might need to do (xxx or "") for other metadata, but so far only had issues with artist
-        local new_media = player.art_url..player.title..(player.artist or "")..player.album..player.length
+        local new_media = p.art_url..p.title..(p.artist or "")..p.album..p.length
 
         -- Media metadata
-        metadata_player.media.art_url = player.art_url or ""
-        metadata_player.media.title = player.title or ""
-        metadata_player.media.artist = player.artist or ""
-        metadata_player.media.album = player.album or ""
-        metadata_player.media.length = player.length or "1"
+        pm.media.art_url = p.art_url or ""
+        pm.media.title = p.title or ""
+        pm.media.artist = p.artist or ""
+        pm.media.album = p.album or ""
+        pm.media.length = p.length or "1"
 
         -- Player metadata
-        metadata_player.player.available = true
-        metadata_player.player.name = player_name or ""
-        metadata_player.player.shuffle = player.shuffle_status or "NONE"
-        metadata_player.player.status = player.playback_status or "PLAYING"
-        metadata_player.player.loop = player.loop_status or "PLAYLIST"
-        metadata_player.player.position = player.position or "1"
-        metadata_player.player.volume = player.volume or "1"
+        pm.player.available = true
+        pm.player.name = p_name or ""
+        pm.player.shuffle = p.shuffle_status or "NONE"
+        pm.player.status = p.playback_status or "PLAYING"
+        pm.player.loop = p.loop_status or "PLAYLIST"
+        pm.player.position = p.position or "1"
+        pm.player.volume = p.volume or "1"
 
         -- Fetch art image and send notification when the media metadata changes
         -- Compare the previously stored metadata to the newly fetched metadata
         if old_media ~= new_media then
-          art_image_fetch(player_name)
+          art_image_fetch(pm)
           -- Don't send a notification on AWM startup
           if old_media ~= "" then
-            track_notification(player_name)
+            track_notification(pm)
           end
         end
-        emit()
       end)
     else
-      metadata_player.player.available = false
+      -- pm.player.available = false
     end
   end
   emit()
@@ -196,12 +186,12 @@ end
 
 metadata_fetch()
 
-local function get_player(by_name)
+local function get_player(name)
   -- Fetch player from mpris itself, not this mpris signal
   -- local mpris_players = mpris.mpris.get_players()
-  for name, player in pairs(players) do
-    if name == by_name then
-      return player
+  for p_name, p in pairs(players) do
+    if p_name == name then
+      return p
     end
   end
 end
@@ -213,7 +203,7 @@ local mpris_timer = gears.timer({
     metadata_fetch()
     -- Speed up the poll if media is currently playing
     local cur_timeout = self.timeout
-    if metadata["global"].player.status == "PLAYING" then
+    if metadata[players["global"]].player.status == "PLAYING" then
       self.timeout = 1
     else
       self.timeout = 5
@@ -224,108 +214,107 @@ local mpris_timer = gears.timer({
   end,
 })
 
-local function mpris_timer_wrapper(callback, override)
+local function mpris_call_wrapper(callback, override)
   mpris_timer:stop()
+  local pm = metadata[players["global"]]
+  local p = players[players["global"]]
   if not override == nil then
     local override_player = get_player(override)
-    callback(override_player)
-  else
-    callback(global_player)
+    pm = metadata[override_player]
+    p = players[override_player]
   end
+  callback(pm, p)
   mpris_timer:start()
 end
 
 local function shuffler(override)
-  mpris_timer_wrapper(function(player)
-    local metadata_player = metadata[player]
-    if metadata_player.player.shuffle == "ON" then
-      metadata_player.player.shuffle = "OFF"
-    elseif metadata_player.player.shuffle == "OFF" then
-      metadata_player.player.shuffle = "ON"
+  mpris_call_wrapper(function(pm, p)
+    if pm.player.shuffle == "ON" then
+      pm.player.shuffle = "OFF"
+    elseif pm.player.shuffle == "OFF" then
+      pm.player.shuffle = "ON"
     end
-    players[player]:shuffle()
+    p:shuffle()
     emit()
   end, override)
 end
 
 local function previouser(override)
-  mpris_timer_wrapper(function(player)
-    players[player]:previous()
+  mpris_call_wrapper(function(_, p)
+    p:previous()
     metadata_fetch()
   end, override)
 end
 
 local function toggler(override)
-  mpris_timer_wrapper(function(player)
-    local metadata_player = metadata[player]
-    if metadata_player.player.status == "PLAYING" then
-      metadata_player.player.status = "PAUSED"
-    elseif metadata_player.player.status == "PAUSED" then
-      metadata_player.player.status = "PLAYING"
+  mpris_call_wrapper(function(pm, p)
+    if pm.player.status == "PLAYING" then
+      pm.player.status = "PAUSED"
+    elseif pm.player.status == "PAUSED" then
+      pm.player.status = "PLAYING"
     end
-    players[player]:play_pause()
+    p:play_pause()
     emit()
   end, override)
 end
 
 local function play_pauser(option, override)
-  mpris_timer_wrapper(function(player)
-    if players[player] == "play" then
-      player:play()
+  mpris_call_wrapper(function(_, p)
+    if option == "play" then
+      p:play()
     elseif option == "pause" then
-      players[player]:pause()
+      p:pause()
     end
   end, override)
 end
 
 local function nexter(override)
-  mpris_timer_wrapper(function(player)
-    players[player]:next()
+  mpris_call_wrapper(function(_, p)
+    p:next()
     metadata_fetch()
   end, override)
 end
 
 local function looper(override)
-  mpris_timer_wrapper(function(player)
-    local metadata_player = metadata[player]
-    if metadata_player.player.loop == "NONE" then
-      metadata_player.player.loop = "PLAYLIST"
-    elseif metadata_player.player.loop == "PLAYLIST" then
-      metadata_player.player.loop = "TRACK"
-    elseif metadata_player.player.loop == "TRACK" then
-      metadata_player.player.loop = "NONE"
+  mpris_call_wrapper(function(pm, p)
+    if pm.player.loop == "NONE" then
+      pm.player.loop = "PLAYLIST"
+    elseif pm.player.loop == "PLAYLIST" then
+      pm.player.loop = "TRACK"
+    elseif pm.player.loop == "TRACK" then
+      pm.player.loop = "NONE"
     end
-    players[player]:loop()
+    p:loop()
     emit()
   end, override)
 end
 
 local function positioner(position_new, override)
-  mpris_timer_wrapper(function(player)
-    players[player].position = h.round(((position_new * metadata[player].media.length) / 100), 3)
+  mpris_call_wrapper(function(pm, p)
+    p.position = h.round(((position_new * pm.media.length) / 100), 3)
   end, override)
 end
 
 local function volumer(volume_new, override)
-  mpris_timer_wrapper(function(player)
+  mpris_call_wrapper(function(pm, p)
     volume_new = h.round((volume_new / 100), 3)
-    players[player].volume = volume_new
-    metadata[player].player.volume = volume_new
+    p.volume = volume_new
+    pm.player.volume = volume_new
     emit()
   end, override)
 end
 
 local function volume_stepper(volume_new, override)
-  mpris_timer_wrapper(function(player)
-    volume_new = (players[player].volume + h.round((volume_new / 100), 3))
-    players[player].volume = volume_new
-    metadata[player].player.volume = volume_new
+  mpris_call_wrapper(function(pm, p)
+    volume_new = (p.volume + h.round((volume_new / 100), 3))
+    p.volume = volume_new
+    pm.player.volume = volume_new
     emit()
   end, override)
 end
 
 awesome.connect_signal("signal::mpris::update", function()
-  mpris_timer_wrapper(function()
+  mpris_call_wrapper(function()
     metadata_fetch()
   end)
 end)
