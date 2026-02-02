80df6f6baa1217fc2dc835b68b791152cc87f8ed
PassiveLemon <lemonl3mn@protonmail.com>
2026-02-01 21:15:22 -0500

    refactor: hilbish nix commands

--- users/lemon/common/home/.config/hilbish/nix.lua
+++ users/lemon/common/home/.config/hilbish/nix.lua
@@ -5,6 +5,7 @@ local fs = require("fs")
 -- Nix helpers
 --
 
+-- General helper functions
 local function find_in_table(table, value)
   for i, v in ipairs(table) do
     if v == value then
@@ -13,14 +14,128 @@ local function find_in_table(table, value)
   end
 end
 
+local function concat_string(str, new)
+  str = str .. new
+end
+
+-- Nix command helper functions
+local function help_check(args, subcommand)
+  local arg1 = tostring(args[1] or "")
+  if (arg1 == "--help") or (arg1 == "-h") then
+    hilbish.run("nix " .. subcommand .. " --help")
+    return 0
+  end
+end
+
+local function build_args(args)
+  local args_str = ""
+  for k, _ in pairs(args) do
+    local arg = tostring(args[k] or "")
+    if string.find(arg, "#") then
+      concat_string(args_str, arg)
+    else
+      concat_string(args_str, " .#" .. arg)
+    end
+    concat_string(args_str, " ")
+  end
+  return args_str
+end
+
+local function simple_arg(args, subcommand)
+  if (#args > 1) and subcommand then
+    hilbish.run("echo 'Too many arguments: " .. subcommand .. "'")
+    return 1
+  end
+  local arg1 = tostring(args[1] or "")
+  local args_str = ".#" .. arg1
+  if string.find(arg1, "#") then
+    args_str = arg1
+  end
+  return args_str
+end
+
+-- Nix related aliases
 hilbish.alias("nos", "nh os switch ~/Documents/GitHub/lemonix")
 hilbish.alias("nhs", "nh home switch ~/Documents/GitHub/lemonix")
-
--- Nix
 hilbish.alias("npr", "nixpkgs-review rev --print-result HEAD")
 hilbish.alias("cma", "comma")
 
--- Well it works, but the point of this was to be faster than nh in terms of eval time and it's so much slower.
+-- Nix build
+commander.register("nb", function(args)
+  help_check(args, "build")
+
+  local args_str = build_args(args)
+
+  hilbish.run("nix build " .. args_str)
+end)
+
+-- Nix develop
+commander.register("nd", function(args)
+  help_check(args, "develop")
+
+  local args_str = simple_arg(args, "nd (installable)")
+
+  hilbish.run("nix develop " .. args_str)
+end)
+
+-- Nix run
+commander.register("nr", function(args)
+  help_check(args, "run")
+
+  local arg_struct = {
+    simple_arg(args),
+    "--",
+    table.concat(args, " ", 2),
+  }
+  local args_str = table.concat(arg_struct, " ")
+
+  hilbish.run("nix run " .. args_str)
+end)
+
+-- Nix shell
+commander.register("ns", function(args)
+  help_check(args, "shell")
+
+  local args_str = build_args(args)
+
+  hilbish.run("nix shell " .. args_str)
+end)
+
+-- Nix flake update
+commander.register("nfu", function(args)
+  help_check(args, "flake update")
+
+  local args_str = table.concat(args, " ")
+
+  hilbish.run("nix flake update " .. args_str)
+end)
+
+-- Nix store prefetch
+commander.register("nsp", function(args)
+  help_check(args, "store prefetch-file")
+
+  local types = { "md5", "sha1", "sha256", "sha512" }
+
+  local type = args[1]
+  local url = args[2]
+
+  if #args < 2 then
+    hilbish.run("echo 'Not enough arguments: nsp (hash-type) (url)'")
+    return 1
+  elseif #args > 2 then
+    hilbish.run("echo 'Too many arguments: nsp (hash-type) (url)'")
+    return 1
+  end
+  if find_in_table(types, type) then
+    hilbish.run("nix store prefetch-file --hash-type " .. type .. " " .. url)
+    return 0
+  else
+    hilbish.run("echo 'Unrecognized hash-type: '" .. type)
+    return 1
+  end
+end)
+
+-- Well it works, but the point of this was to be faster than nh in terms of eval time but it's so much slower.
 -- commander.register("nos", function()
 --   print("Building NixOS configuration...")
 --   local code1 = hilbish.run("sudo -v")
@@ -59,124 +174,4 @@ hilbish.alias("cma", "comma")
 --     print("Failed to elevate priviledges.")
 --   end
 -- end)
-
--- Nix build
-commander.register("nb", function(args)
-  if (args[1] == "--help") or (args[1] == "-h") then
-    hilbish.run("nix build --help")
-    return 0
-  end
-
-  local args_str = ""
-  for k, _ in pairs(args) do
-    if string.find(tostring(args[k]), "#") then
-      args_str = args_str .. tostring(args[k])
-    else
-      args_str = args_str .. " .#" .. tostring((args[k] or ""))
-    end
-  end
-
-  hilbish.run("nix build " .. args_str)
-end)
-
--- Nix develop
-commander.register("nd", function(args)
-  if #args > 1 then
-    hilbish.run("echo 'Too many arguments: nd (installable)'")
-    return 1
-  end
-  if (args[1] == "--help") or (args[1] == "-h") then
-    hilbish.run("nix develop --help")
-    return 0
-  end
-
-  local args_str = ""
-  if string.find(tostring(args[1]), "#") then
-    args_str = tostring(args[1])
-  else
-    args_str = ".#" .. tostring((args[1] or ""))
-  end
-
-  hilbish.run("nix develop " .. args_str)
-end)
-
--- Nix run
-commander.register("nr", function(args)
-  if (args[1] == "--help") or (args[1] == "-h") then
-    hilbish.run("nix run --help")
-    return 0
-  end
-
-  local args_str = ""
-  if string.find(tostring(args[1]), "#") then
-    args_str = tostring(args[1])
-  else
-    args_str = ".#" .. tostring((args[1] or ""))
-  end
-  args_str = args_str .. " --"
-  for k, _ in pairs(args) do
-    if k > 1 then
-      args_str = args_str .. " " .. tostring(args[k])
-    end
-  end
-
-  hilbish.run("nix run " .. args_str)
-end)
-
--- Nix shell
-commander.register("ns", function(args)
-  if (args[1] == "--help") or (args[1] == "-h") then
-    hilbish.run("nix shell --help")
-    return 0
-  end
-
-  local args_str = ""
-  for k, _ in pairs(args) do
-    if string.find(tostring(args[k]), "#") then
-      args_str = args_str .. tostring(args[k])
-    else
-      args_str = args_str .. " nixpkgs#" .. tostring((args[k] or ""))
-    end
-  end
-
-  hilbish.run("nix shell" .. args_str)
-end)
-
--- Nix flake update
-commander.register("nfu", function(args)
-  if (args[1] == "--help") or (args[1] == "-h") then
-    hilbish.run("nix flake update --help")
-    return 0
-  end
-
-  local args_str = ""
-  for k, _ in pairs(args) do
-    args_str = args_str .. " " .. tostring(args[k])
-  end
-
-  hilbish.run("nix flake update" .. args_str)
-end)
-
--- Nix store prefetch
-commander.register("nsp", function(args)
-  local types = { "md5", "sha1", "sha256", "sha512" }
-
-  local type = args[1]
-  local url = args[2]
-
-  if #args < 2 then
-    hilbish.run("echo 'Not enough arguments: nsp (hash-type) (url)'")
-    return 1
-  elseif #args > 2 then
-    hilbish.run("echo 'Too many arguments: nsp (hash-type) (url)'")
-    return 1
-  end
-  if find_in_table(types, type) then
-    hilbish.run("nix store prefetch-file --hash-type " .. type .. " " .. url)
-    return 0
-  else
-    hilbish.run("echo 'Unrecognized hash-type: '" .. type)
-    return 1
-  end
-end)
 
