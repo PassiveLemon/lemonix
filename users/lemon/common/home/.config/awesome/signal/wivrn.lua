fb37627ec48ee1e30c1908b70a2d1dfe58c243b4
PassiveLemon <lemonl3mn@protonmail.com>
2026-06-25 20:46:12 -0400

    refactor: wivrn signal
    Untested, but the connected values look good

--- users/lemon/common/home/.config/awesome/signal/wivrn.lua
+++ users/lemon/common/home/.config/awesome/signal/wivrn.lua
@@ -1,9 +1,14 @@
 local awful = require("awful")
 local gears = require("gears")
 
--- TODO: Use lua dbus instead of busctl
+local lgi = require("lgi")
+local gio = lgi.require("Gio")
+local glib = lgi.GLib
 
-local state_cache = false
+local bus = gio.bus_get_sync(gio.BusType.SESSION)
+
+-- State
+local bus_state = false
 
 local function focus_steam()
   for _, c in ipairs(client.get()) do
@@ -20,29 +25,41 @@ local function focus_steam()
 end
 
 local function wivrn()
-  awful.spawn.easy_async("busctl --user get-property io.github.wivrn.Server /io/github/wivrn/Server io.github.wivrn.Server HeadsetConnected", function(wivrn_stdout)
-    local wivrn_bus = wivrn_stdout:gsub("\n", "")
-    if wivrn_bus == "b true" then
-      if not state_cache then
-        state_cache = true
-        awful.spawn.with_shell("kill $(pgrep picom)")
-        awful.spawn.with_shell("kill $(pgrep easyeffects)")
-        focus_steam()
-      end
-    else
-      if state_cache then
-        state_cache = false
-        awful.spawn.with_shell("pidof -q picom || picom --realtime -b")
-        awful.spawn.with_shell("pidof -q easyeffects || easyeffects --gapplication-service")
-      end
+  local result = bus:call_sync(
+    "io.github.wivrn.Server",
+    "/io/github/wivrn/Server",
+    "org.freedesktop.DBus.Properties",
+    "Get",
+    glib.Variant("(ss)", {
+      "io.github.wivrn.Server",
+      "HeadsetConnected"
+    }),
+    nil,
+    gio.DBusCallFlags.NONE,
+    -1)
+
+  local connected = result:get_child_value(0):get_boolean()
+
+  if connected then
+    if not bus_state then
+      bus_state = true
+      awful.spawn.with_shell("systemctl --user stop picom")
+      awful.spawn.with_shell("systemctl --user stop easyeffects")
+      focus_steam()
     end
-  end)
+  else
+    if bus_state then
+      bus_state = false
+      awful.spawn.with_shell("systemctl --user restart picom")
+      awful.spawn.with_shell("systemctl --user restart easyeffects")
+    end
+  end
 end
 
 -- luacheck: ignore 211
 ---@diagnostic disable-next-line: unused-local
 local wivrn_timer = gears.timer({
-  timeout = 2,
+  timeout = 1,
   autostart = true,
   callback = function()
     wivrn()
