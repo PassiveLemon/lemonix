0870c715098a55330a751b1d886b71d4840ae43d
PassiveLemon <lemonl3mn@protonmail.com>
2026-08-12 14:56:40 -0400

    refactor: event drive power signal

--- users/lemon/common/home/.config/awesome/signal/power.lua
+++ users/lemon/common/home/.config/awesome/signal/power.lua
@@ -1,5 +1,4 @@
 local awful = require("awful")
-local gears = require("gears")
 local naughty = require("naughty")
 
 local upower = require("lgi").require("UPowerGlib")
@@ -27,32 +26,33 @@ end
 
 local system_battery_good = true
 
-local function power()
-  ac = line.online
-  perc = bat.percentage
-  time = bat.time_to_empty
-  emit()
-
-  -- Manage device battery
-  if (perc > 10) and not system_battery_good then
+local function power_manage()
+  -- Reset battery state if charged enough or plugged in
+  if ac or ((perc > 10) and not system_battery_good) then
     system_battery_good = true
   end
+  -- If not plugged in, battery is less than 10%, and state is still "good" then warn
   if not ac and (perc <= 10) and system_battery_good then
     naughty.notify({ title = "System battery low (10%)" })
     system_battery_good = false
   end
+  -- If not plugged in and battery drops below 5%, then suspend
   if not ac and (perc <= 5) then
     awful.spawn("systemctl suspend")
   end
 end
 
--- luacheck: ignore 211
----@diagnostic disable-next-line: unused-local
-local power_timer = gears.timer({
-  timeout = 5,
-  autostart = true,
-  callback = function()
-    power()
-  end,
-})
+line.on_notify["online"] = function(self)
+  ac = self.online
+  emit()
+end
+
+bat.on_notify["percentage"] = function(self)
+  perc = self.percentage
+  time = self.time_to_empty
+  emit()
+  if not ac then
+    power_manage()
+  end
+end
 
