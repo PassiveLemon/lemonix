7884629196deeb955335bdcd23e12fe60121f142
PassiveLemon <lemonl3mn@protonmail.com>
2026-08-18 16:45:24 -0400

    refactor: event drive wivrn signal

--- users/lemon/common/home/.config/awesome/signal/wivrn.lua
+++ users/lemon/common/home/.config/awesome/signal/wivrn.lua
@@ -1,14 +1,12 @@
 local awful = require("awful")
-local gears = require("gears")
 
-local lgi = require("lgi")
-local gio = lgi.require("Gio")
-local glib = lgi.GLib
-
-local bus = gio.bus_get_sync(gio.BusType.SESSION)
+local ast_Wp = require("lgi").require("AstalWp")
+local wp = ast_Wp.get_default()
+local audio = wp.audio
 
 -- State
-local bus_state = false
+local cached_speaker = audio.default_speaker
+local cached_microphone = audio.default_microphone
 
 local function focus_steam()
   for _, c in ipairs(client.get()) do
@@ -24,45 +22,39 @@ local function focus_steam()
   end
 end
 
-local function wivrn()
-  local result = bus:call_sync(
-    "io.github.wivrn.Server",
-    "/io/github/wivrn/Server",
-    "org.freedesktop.DBus.Properties",
-    "Get",
-    glib.Variant("(ss)", {
-      "io.github.wivrn.Server",
-      "HeadsetConnected"
-    }),
-    nil,
-    gio.DBusCallFlags.NONE,
-    -1)
+local function wivrn_connected()
+  awful.spawn("systemctl --user stop picom")
+  awful.spawn("systemctl --user stop easyeffects")
+  focus_steam()
+end
 
-  local connected = result:get_child_value(0):get_boolean()
+local function wivrn_disconnected()
+  awful.spawn("systemctl --user restart picom")
+  awful.spawn("systemctl --user restart easyeffects")
+end
 
-  if connected then
-    if not bus_state then
-      bus_state = true
-      awful.spawn.with_shell("systemctl --user stop picom")
-      awful.spawn.with_shell("systemctl --user stop easyeffects")
-      focus_steam()
-    end
-  else
-    if bus_state then
-      bus_state = false
-      awful.spawn.with_shell("systemctl --user restart picom")
-      awful.spawn.with_shell("systemctl --user restart easyeffects")
-    end
+function wp:on_node_added(node)
+  local id = node.id
+  local name = node.name
+  if name == "wivrn.sink" then
+    cached_speaker = audio.default_speaker
+    local speaker = audio:get_speaker(id)
+    speaker:set_is_default(true)
+    wivrn_connected()
+  elseif name == "wivrn.source" then
+    cached_microphone = audio.default_microphone
+    local microphone = audio:get_microphone(id)
+    microphone:set_is_default(true)
   end
 end
 
--- luacheck: ignore 211
----@diagnostic disable-next-line: unused-local
-local wivrn_timer = gears.timer({
-  timeout = 1,
-  autostart = true,
-  callback = function()
-    wivrn()
-  end,
-})
+function wp:on_node_removed(node)
+  local name = node.name
+  if name == "wivrn.sink" then
+    cached_speaker:set_is_default(true)
+    wivrn_disconnected()
+  elseif name == "wivrn.source" then
+    cached_microphone:set_is_default(true)
+  end
+end
 
