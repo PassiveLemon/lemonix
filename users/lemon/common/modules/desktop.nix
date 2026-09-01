57c81a85b94b86e4607d79a969126d4ac1954898
PassiveLemon <lemonl3mn@protonmail.com>
2026-09-01 17:29:21 -0400

    fix: replace bling with tofi and flameshot with awful.screenshot
    Maybe I'll fix bling up to work with SomeWM but for now it is going to be removed because it causes problems. Similarly, flameshot also causes problems so use the native awful.screenshot

--- users/lemon/common/modules/desktop.nix
+++ users/lemon/common/modules/desktop.nix
@@ -3,6 +3,13 @@
     inputs.lemonake.homeModules.somewm
   ];
 
+  home.packages = with pkgs; [
+    wlr-randr xhost
+    resources baobab
+    gparted qdiskinfo
+    ffmpegthumbnailer # https://github.com/NixOS/nixpkgs/pull/509742
+  ];
+
   wayland = {
     # windowManager.somewm = {
     #   enable = true;
@@ -28,24 +35,40 @@
     };
   };
 
+  programs = {
+    tofi = {
+      enable = true;
+      settings = {
+        drun-launch = true;
+        font = "monospace";
+        font-size = 12;
+        height = 500;
+        width = 350;
+        result-spacing = 4;
+        prompt-text = ">";
+        text-color = "#aaaaaa";
+        background-color = "#222222";
+        selection-color = "#dcdcdc";
+        selection-background = "#363636";
+        selection-background-padding = "2, 8, 2, 8";
+        selection-background-corner-radius = 12;
+        default-result-color = "#aaaaaa";
+        default-result-background = "#292929";
+        default-result-background-padding = "2, 8, 2, 8";
+        default-result-background-corner-radius = 12;
+        border-width = 3;
+        border-color = "#40454f";
+        outline-width = 0;
+        padding-left = 8;
+        padding-top = 8;
+        corner-radius = 12;
+      };
+    };
+  };
+
   services = {
     trayscale.enable = true;
     network-manager-applet.enable = true;
-    flameshot = {
-      enable = true;
-      settings = {
-        General = {
-          disabledTrayIcon = true;
-          showStartupLaunchMessage = false;
-          showDesktopNotification = false;
-          filenamePattern = "%Y-%m-%d_%H-%M-%S_%b-%d";
-          saveAsFileExtension = "png";
-          savePath = "/home/lemon/Pictures/Flameshot";
-          captureActiveMonitor = true;
-          useX11LegacyScreenshot = true;
-        };
-      };
-    };
   };
 }
 
